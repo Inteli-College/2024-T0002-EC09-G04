3ef254cae136cc5e687a9ea668e2360fadc67538
Henrique Marlon <henriquemarloncs@gmail.com>
2024-02-08 11:51:20 -0300

    some changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  baseUrl: `/${organizationName}/`,
 
   projectName: '2024-T0002-EC09-G04',
   organizationName: 'docs',
