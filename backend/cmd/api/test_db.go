85b59540e435cf9e816aa4fdcac5c2242675cfd9
gabrdgs <99189378+gabInteli@users.noreply.github.com>
2024-03-04 03:06:30 -0300

    update and fix api

--- backend/cmd/api/test_db.go
+++ backend/cmd/api/test_db.go
@@ -10,7 +10,8 @@ import (
 
 func insertMockData(db *sql.DB) error {
 	// Preparar a declaração SQL de inserção
-	stmt, err := db.Prepare("INSERT INTO Gas (id_gas, id_estacao, CO2, CO, NO2, MP10, MP25) VALUES ($1, $2, $3, $4, $5, $6, $7)")
+	stmt, err := db.Prepare("INSERT INTO Gas (CO2, CO, NO2, MP10, MP25) VALUES ($1, $2, $3, $4, $5)")
+
 	if err != nil {
 		return err
 	}
@@ -18,21 +19,20 @@ func insertMockData(db *sql.DB) error {
 
 	// Dados mockados para inserção
 	mockData := []struct {
-		IDEstacao int
 		CO2       float64
 		CO        float64
 		NO2       float64
 		MP10      float64
 		MP25      float64
 	}{
-		{1, 100, 50, 25, 10, 5},
-		{1, 150, 60, 30, 15, 8},
-		{1, 200, 70, 35, 20, 10},
+		{100, 50, 25, 10, 5},
+		{150, 60, 30, 15, 8},
+		{200, 70, 35, 20, 10},
 	}
 
 	// Inserir dados mockados na tabela
 	for _, data := range mockData {
-		_, err := stmt.Exec(4, data.IDEstacao, data.CO2, data.CO, data.NO2, data.MP10, data.MP25)
+		_, err := stmt.Exec(data.CO2, data.CO, data.NO2, data.MP10, data.MP25)
 		if err != nil {
 			return err
 		}
