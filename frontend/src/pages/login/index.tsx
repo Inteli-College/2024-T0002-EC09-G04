d6b04d51d9af8ba3372e5197703941a9273bf72c
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2024-03-12 09:57:23 -0300

    update library

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import Image from 'next/image'
-//import Input from '../../components/LoginInput/LoginInput';
-import { Input } from "./../../components/Input/Input"
-import '../../app/globals.css';
+import Image from 'next/image';
+import {Input}  from 'antd'
+import './../../app/globals.css';
 
+import logo from '../../assets/img/id_visual.png';
 
-
-export default function login(){
+export default function Login() {
   return (
     <div className="flex h-screen justify-center items-center bg-custom-blue">
       <div className="flex flex-col gap-4">
-        <Image src={}/>
-      </div>
-      <div className="flex flex-col gap-4">
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          alt="Logo da empresa"
+        />
         <Input placeholder="USUÁRIO" type="text" name="username" />
         <Input placeholder="SENHA" type="password" name="password" />
       </div>
     </div>
   );
-};
-
+}
