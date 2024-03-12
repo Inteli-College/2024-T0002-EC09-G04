c1ee0f7712d7f9eb071fe3ac89ead764899902cd
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2024-03-12 08:58:51 -0300

    update library

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Input from '../../components/LoginInput/LoginInput';
+import Image from 'next/image'
+//import Input from '../../components/LoginInput/LoginInput';
+import { Input } from "./../../components/Input/Input"
 import '../../app/globals.css';
+
+
 
 export default function login(){
   return (
     <div className="flex h-screen justify-center items-center bg-custom-blue">
+      <div className="flex flex-col gap-4">
+        <Image src={}/>
+      </div>
       <div className="flex flex-col gap-4">
         <Input placeholder="USUÁRIO" type="text" name="username" />
         <Input placeholder="SENHA" type="password" name="password" />
