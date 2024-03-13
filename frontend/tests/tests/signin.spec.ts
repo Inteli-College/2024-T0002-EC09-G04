4701ace469019cb952c9c40f5ac57307bccf1d96
Pablo Ruan <pablo.viana@sou.inteli.edu.br>
2024-03-13 11:27:42 -0300

    test(frontend): :white_check_mark: integrate login tests with frontend

--- frontend/tests/tests/signin.spec.ts
+++ frontend/tests/tests/signin.spec.ts
@@ -6,46 +6,62 @@ test.beforeEach(async ({ page }) => {
 });  
 
 test.describe('Signin', () => {
-    test('should verify if input username is visible', async ({ page }) => {
+    test('input username should be visible', async ({ page }) => {
         const usernameInput = await page.waitForSelector('input[id="Email"]');
         expect(usernameInput).not.toBeNull();
     });
-    test('should verify if input password is visible', async ({ page }) => {
+    test('input password should be visible', async ({ page }) => {
         const passwordInput = await page.waitForSelector('input[id="password"]');
         expect(passwordInput).not.toBeNull();
     });
-    test('should verify if login button is visible', async ({ page }) => {
+    test('login button should be visible', async ({ page }) => {
         const loginButton = await page.waitForSelector('button[type="submit"]');
         expect(loginButton).not.toBeNull();
     });
-    test('should verify if login button is disabled', async ({ page }) => {
-        const loginButton = await page.waitForSelector('button[type="submit"]');
-        expect(await loginButton.isEnabled()).toBeFalsy();
+    test('password should be hidden', async ({ page }) => {
+        const passwordInput = await page.waitForSelector('input[id="password"]');
+        expect(await passwordInput.getAttribute('type')).toBe('password');
     });
-    test('should verify if login button is enabled', async ({ page }) => {
-        await page.fill('input[id="Email"]', 'testing123');
-        await page.fill('input[id="password"]', 'testing123');
-        const loginButton = await page.waitForSelector('button[type="submit"]');
-        expect(await loginButton.isEnabled()).toBeTruthy();
-    });
-    test('should verify if login button is disabled after clearing username input', async ({ page }) => {
+    // test('login button should be disabled', async ({ page }) => {
+    //     const loginButton = await page.waitForSelector('button[type="submit"]');
+    //     expect(await loginButton.isEnabled()).toBeFalsy();
+    // });
+    // test('login button should be enabled', async ({ page }) => {
+    //     await page.fill('input[id="Email"]', 'testing123');
+    //     await page.fill('input[id="password"]', 'testing123');
+    //     const loginButton = await page.waitForSelector('button[type="submit"]');
+    //     expect(await loginButton.isEnabled()).toBeTruthy();
+    // });
+    // test('login button should be disabled after clearing username input', async ({ page }) => {
+    //     await page.fill('input[id="Email"]', 'testing123');
+    //     await page.fill('input[id="password"]', 'testing123');
+    //     await page.fill('input[id="Email"]', '');
+    //     const loginButton = await page.waitForSelector('button[type="submit"]');
+    //     expect(await loginButton.isEnabled()).toBeFalsy();
+    // });
+    test('message should appear after clearing username input', async ({ page }) => {
         await page.fill('input[id="Email"]', 'testing123');
         await page.fill('input[id="password"]', 'testing123');
         await page.fill('input[id="Email"]', '');
-        const loginButton = await page.waitForSelector('button[type="submit"]');
-        expect(await loginButton.isEnabled()).toBeFalsy();
+        expect(await expect(page.locator('div[class="ant-form-item-explain-error"]')).toHaveText('Por favor insira seu email!'));
     });
-    test('should verify if login button is disabled after clearing password input', async ({ page }) => {
+    // test('login button should be disabled after clearing password input', async ({ page }) => {
+    //     await page.fill('input[id="Email"]', 'testing123');
+    //     await page.fill('input[id="password"]', 'testing123');
+    //     await page.fill('input[id="password"]', '');
+    //     const loginButton = await page.waitForSelector('button[type="submit"]');
+    //     expect(await loginButton.isEnabled()).toBeFalsy();
+    // });
+    test('message should appear after clearing password input', async ({ page }) => {
         await page.fill('input[id="Email"]', 'testing123');
         await page.fill('input[id="password"]', 'testing123');
         await page.fill('input[id="password"]', '');
-        const loginButton = await page.waitForSelector('button[type="submit"]');
-        expect(await loginButton.isEnabled()).toBeFalsy();
+        expect(await expect(page.locator('div[class="ant-form-item-explain-error"]')).toHaveText('Por favor digite sua senha!'));
     });
-    test('should verify if url changes after clicking login button', async ({ page }) => {
-        await page.fill('input[id="Email"]', 'testing123');
-        await page.fill('input[id="password"]', 'testing123');
-        await page.click('button[type="submit"]');
-        expect(page.url()).not.toBe(url);
-    });
+    // test('url should change after clicking login button', async ({ page }) => {
+    //     await page.fill('input[id="Email"]', 'testing123');
+    //     await page.fill('input[id="password"]', 'testing123');
+    //     await page.click('button[type="submit"]');
+    //     expect(page.url()).not.toBe(url);
+    // });
 });
