d4f9feb25dd8a43b2c707f6dd9244167ff789729
amandafontes <amanda.fontes@sou.inteli.edu.br>
2024-03-27 11:37:51 -0300

    feat: desacoplamento da construção da simulação

--- backend/cmd/simulation/main.go
+++ backend/cmd/simulation/main.go
@@ -10,12 +10,14 @@ import (
 	"time"
 
 	"github.com/Inteli-College/2024-T0002-EC09-G04/backend/internal/domain/entity"
+	"github.com/Inteli-College/2024-T0002-EC09-G04/backend/internal/infra/repository"
 	"github.com/Inteli-College/2024-T0002-EC09-G04/backend/internal/usecase"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
-	"github.com/Inteli-College/2024-T0002-EC09-G04/backend/internal/infra/repository"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var mutex sync.Mutex
 
 func main() {
 	options := options.Client().ApplyURI(
@@ -37,48 +39,87 @@ func main() {
 	repository := repository.NewSensorRepositoryMongo(client, "mongodb", "sensors")
 	findAllSensorsUseCase := usecase.NewFindAllSensorsUseCase(repository)
 
+	knownSensors := make(map[string]bool)
+
+	var wg sync.WaitGroup
+
 	sensors, err := findAllSensorsUseCase.Execute()
 	if err != nil {
 		log.Fatalf("Failed to find all sensors: %v", err)
 	}
 
-	var wg sync.WaitGroup
 	for _, sensor := range sensors {
+		knownSensors[sensor.ID] = true
 		wg.Add(1)
-		log.Printf("Starting sensor: %v", sensor)
-		go func(sensor usecase.FindAllSensorsOutputDTO) {
-			defer wg.Done()
-			opts := MQTT.NewClientOptions().AddBroker(
-				fmt.Sprintf("ssl://%s:%s",
-					os.Getenv("BROKER_TLS_URL"),
-					os.Getenv("BROKER_PORT"))).SetUsername(
-				os.Getenv("BROKER_USERNAME")).SetPassword(
-				os.Getenv("BROKER_PASSWORD")).SetClientID(sensor.ID)
-			client := MQTT.NewClient(opts)
-			if session := client.Connect(); session.Wait() && session.Error() != nil {
-				log.Fatalf("Failed to connect to MQTT broker: %v", session.Error())
-			}
-			for {
-				payload, err := entity.NewSensorPayload(
-					sensor.ID,
-					sensor.Params,
-					time.Now(),
-				)
-				if err != nil {
-					log.Fatalf("Failed to create payload: %v", err)
-				}
-
-				jsonBytesPayload, err := json.Marshal(payload)
-				if err != nil {
-					log.Println("Error converting to JSON:", err)
-				}
-
-				token := client.Publish("sensors", 1, false, string(jsonBytesPayload))
-				log.Printf("Published: %s, on topic: %s", string(jsonBytesPayload), "sensors")
-				token.Wait()
-				time.Sleep(120 * time.Second)
-			}
-		}(sensor)
+		log.Printf("Starting initial sensor simulation: %v", sensor)
+		go simulateSensor(sensor, &wg)
 	}
+
+	wg.Add(1)
+	go checkForNewSensors(findAllSensorsUseCase, knownSensors, &wg)
+
 	wg.Wait()
+}
+
+func checkForNewSensors(useCase *usecase.FindAllSensorsUseCase, knownSensors map[string]bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		sensors, err := useCase.Execute()
+		if err != nil {
+			log.Printf("Failed to find all sensors: %v", err)
+			time.Sleep(1 * time.Minute)
+			continue
+		}
+
+		mutex.Lock()
+		for _, sensor := range sensors {
+			if _, exists := knownSensors[sensor.ID]; !exists {
+				wg.Add(1)
+				go simulateSensor(sensor, wg)
+				knownSensors[sensor.ID] = true
+			}
+		}
+		mutex.Unlock()
+
+		time.Sleep(10 * time.Minute)
+	}
+}
+
+func simulateSensor(sensor usecase.FindAllSensorsOutputDTO, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	opts := MQTT.NewClientOptions().AddBroker(
+		fmt.Sprintf("ssl://%s:%s",
+			os.Getenv("BROKER_TLS_URL"),
+			os.Getenv("BROKER_PORT"))).SetUsername(
+		os.Getenv("BROKER_USERNAME")).SetPassword(
+		os.Getenv("BROKER_PASSWORD")).SetClientID(sensor.ID)
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to connect to MQTT broker for sensor %s: %v", sensor.ID, token.Error())
+	}
+
+	for {
+		payload, err := entity.NewSensorPayload(
+			sensor.ID,
+			sensor.Params,
+			time.Now(),
+		)
+		if err != nil {
+			log.Fatalf("Failed to create payload for sensor %s: %v", sensor.ID, err)
+		}
+
+		jsonBytesPayload, err := json.Marshal(payload)
+		if err != nil {
+			log.Printf("Error converting payload to JSON for sensor %s: %v", sensor.ID, err)
+			continue
+		}
+
+		token := client.Publish("sensors", 1, false, string(jsonBytesPayload))
+		log.Printf("Published for sensor %s, payload: %s", sensor.ID, string(jsonBytesPayload))
+		token.Wait()
+
+		time.Sleep(120 * time.Second)
+	}
 }
